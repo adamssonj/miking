df98e6ee2a08db0907c43bb00cfed2922581eb1d
Johan Adamsson <johadamsson@gmail.com>
2023-04-21 15:42:17 +0200

    Remove print

--- src/boot/lib/inter.ml
+++ src/boot/lib/inter.ml
@@ -8,7 +8,6 @@ let registry = ref None
 let register m = registry := Some m
 
 let get_plugin () : (module PLUG)  =
-  print_endline "...2.3.234.2" ;
   match !registry with 
   | Some s -> s
   | None -> failwith "No plugin loaded" 
